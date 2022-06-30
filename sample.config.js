5eb1b1fe5d568fc93402d22d111953b719c3c308
brianleect <63389110+brianleect@users.noreply.github.com>
2022-06-30 20:41:41 +0800

    init base files w packages

---
+++ sample.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //PROVIDER: 'https://eth-mainnet.alchemyapi.io/v2/API_KEY_HERE',
+    //PROVIDER: 'http://localhost:8545', // If running on same machine
+    // Rate limit params
+    RATELIMIT_CALL: 10, // Set rate limit of calls per second. Set to 
+}
